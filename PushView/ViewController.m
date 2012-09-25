19f3cf9e77cc8f647c464e5c3daa3df07323c3ca
Elijah Buters <ElijahButers@gmai.com>
2012-09-25 20:02:34 +0400

    Push View

--- PushView/ViewController.m
+++ PushView/ViewController.m
@@ -18,12 +18,44 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    array = [[NSMutableArray alloc]init];
+    
+    [array addObject:@"Number One"];
+    [array addObject:@"Number Two"];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [array count];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *cellIdentifier = @"Cell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text  = [array objectAtIndex:[indexPath row]];
+    return  cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewController *viewController = [[UITableViewController alloc] init];
+    [self.navigationController pushViewController:viewController animated:YES];
+    
 }
 
 @end
